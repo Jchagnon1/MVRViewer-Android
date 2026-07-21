6df64acfc4cb5261f6cdee8baf5c0abac40c3c0d
Jean-Maxence <chagnon.jeanmaxence@gmail.com>
2026-07-21 20:03:00 +0200

    Version 0.9 (9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.minou.mvrviewer"
         minSdk = 28  // requis par SceneView/Filament (vue 3D)
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.8"
+        versionCode = 9
+        versionName = "0.9"
         vectorDrawables { useSupportLibrary = true }
     }
 
